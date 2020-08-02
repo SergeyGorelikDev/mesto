3b6cb81daeec46c8b0072ccf147eda32c7e830b6
Sergey Gorelik <yasergeygorelik@yandex.ru>
2020-08-02 17:25:20 +0300

    fix: Исправить замечания по верстке и скрипту (код ревью)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,17 +2,16 @@ let container = document.querySelector('.page');
 let popUp = container.querySelector('.popup');
 let editButton = container.querySelector('.profile__edit-button');
 let closeButton = container.querySelector('.popup__close-button');
-let sumbitButton = container.querySelector('.popup__submit-button');
+let inputForm = container.querySelector('.popup__input-form');
 let profileName = container.querySelector('.profile__title');
 let profileAbout = container.querySelector('.profile__subtitle');
+let nameInput = container.querySelector('.popup__input_type_name');
+let jobInput = container.querySelector('.popup__input_type_about');
 
 function formEditHandler() {
-    let popUpName = container.querySelector('.popup__input_type_name');
-    let popUpAbout = container.querySelector('.popup__input_type_about');
-    popUpName.value = profileName.textContent;
-    popUpAbout.value = profileAbout.textContent;
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAbout.textContent;
     popUp.classList.add('popup_opened');
-    
 }
 
 function formCloseHandler() {
@@ -21,15 +20,12 @@ function formCloseHandler() {
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-
-    let nameInput = container.querySelector('.popup__input_type_name');
-    let jobInput = container.querySelector('.popup__input_type_about');
-
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
-    popUp.classList.remove('popup_opened');
+    formCloseHandler();
  }
 
 editButton.addEventListener('click',formEditHandler);
 closeButton.addEventListener('click',formCloseHandler);
-sumbitButton.addEventListener('click',formSubmitHandler);
+console.log(inputForm);
+inputForm.addEventListener('submit',formSubmitHandler);
